d5c27cf26e681cdb1d9902b9253bfe4022854a20
Mikhail Bulygin <bulyginmv1996@gmail.com>
2019-01-10 15:54:33 -0800

    Revert "Update InstaTagger.py"
    This reverts commit f0be66e226cf38e327e6bcb2f89d06614e88d121.

--- InstaTagger.py
+++ InstaTagger.py
@@ -1,4 +1,14 @@
 
+# coding: utf-8
+
+# # Object Detection Demo
+# Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md) before you start.
+
+# # Imports
+
+# In[35]:
+
+
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -8,10 +18,11 @@ import tensorflow as tf
 from urllib.request import urlopen
 from distutils.version import StrictVersion
 import csv
-#function created to augmentify the number of tags
 import augmentation as au
+from matplotlib import pyplot as plt
 from PIL import Image
 
+# This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
 
@@ -19,24 +30,47 @@ if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
 
+# ## Env setup
 
+# In[36]:
+
+# ## Object detection imhttps://dmv.ny.gov/forms/mv80u1.pdfports
+# Here are the imports from the object detection module.
+
+# In[37]:
 
 
 from object_detection.utils import label_map_util
 
+from object_detection.utils import visualization_utils as vis_util
 
 
+# # Model preparation
 
+# ## Variables
+#
+# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_FROZEN_GRAPH` to point to a new .pb file.
+#
+# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+
+# In[38]:
+
+
+# What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
 
+# List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 
+# ## Download Model
 
+# In[39]:
 
 
 opener = urllib.request.URLopener()
@@ -48,7 +82,9 @@ for file in tar_file.getmembers():
     tar_file.extract(file, os.getcwd())
 
 
+# ## Load a (frozen) Tensorflow model into memory.
 
+# In[40]:
 
 
 detection_graph = tf.Graph()
@@ -60,11 +96,18 @@ with detection_graph.as_default():
     tf.import_graph_def(od_graph_def, name='')
 
 
+# ## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+# In[41]:
 
 
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
 
+# ## Helper code
+
+# In[42]:
 
 
 def load_image_into_numpy_array(image):
@@ -73,12 +116,15 @@ def load_image_into_numpy_array(image):
       (im_height, im_width, 3)).astype(np.uint8)
 
 
+# # Detection
 
-
-#add path to csv file containing links to images
+# In[43]:
 csv_file='C:\python\ex_1.csv'
 
-# function to take links to images from csv file
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
 def get_images_from(csv_file):
     reader = csv.reader(open(csv_file, 'r'),delimiter=';')
     link_list=[]
@@ -88,16 +134,19 @@ def get_images_from(csv_file):
     del link_list[0]
     return link_list
 
-IMAGE_PATHS = get_images_from(csv_file)
+TEST_IMAGE_PATHS = get_images_from(csv_file)
 
+# Size, in inches, of the output images.
 IMAGE_SIZE = (12, 8)
 
 
+# In[44]:
 
 
 def run_inference_for_single_image(image, graph):
   with graph.as_default():
     with tf.Session() as sess:
+      # Get handles to input and output tensors
       ops = tf.get_default_graph().get_operations()
       all_tensor_names = {output.name for op in ops for output in op.outputs}
       tensor_dict = {}
@@ -110,8 +159,10 @@ def run_inference_for_single_image(image, graph):
           tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
               tensor_name)
       if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
         detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
         detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
         real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
         detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
         detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
@@ -119,30 +170,42 @@ def run_inference_for_single_image(image, graph):
             detection_masks, detection_boxes, image.shape[0], image.shape[1])
         detection_masks_reframed = tf.cast(
             tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
         tensor_dict['detection_masks'] = tf.expand_dims(
             detection_masks_reframed, 0)
       image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
+      # Run inference
       output_dict = sess.run(tensor_dict,
                              feed_dict={image_tensor: np.expand_dims(image, 0)})
 
-
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
       output_dict['detection_classes'] = output_dict[
           'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
       output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
   return output_dict
 
 
+# In[49]:
 
 
-for image_path in IMAGE_PATHS:
+for image_path in TEST_IMAGE_PATHS:
   out=[]
   image = Image.open(urlopen(image_path))
+  # the array based representation of the image will be used later in order to prepare the
+  # result image with boxes and labels on it.
   image_np = load_image_into_numpy_array(image)
+  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
   image_np_expanded = np.expand_dims(image_np, axis=0)
+  # Actual detection.
   output_dict = run_inference_for_single_image(image_np, detection_graph)
-  # create a list containing image link, augmented itemes detected and confidece of the model.
+  # Visualization of the results of a detection.
   for index,value in enumerate(output_dict['detection_classes']): 
-      if output_dict['detection_scores'][index] > 0.5 and category_index.get(value)['name'] not in out:
+      if output_dict['detection_scores'][index] > 0.5:
+              if category_index.get(value)['name'] not in out:
                   out.append(au.tag_aug(category_index.get(value)['name'],output_dict['detection_scores'][index]*100))
   print (image_path,out)  
